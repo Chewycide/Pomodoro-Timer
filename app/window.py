9b85e48170d53997db6f1a3cce3e3739ac099bdc
Chewycide <gitchewycide@gmail.com>
2023-01-20 10:23:57 +0800

    Refactor Code

--- app/window.py
+++ app/window.py
@@ -57,9 +57,9 @@ class Pomodoro(QWidget):
         row_1 = QHBoxLayout()
         row_1.setAlignment(Qt.AlignCenter)
 
-        self.timer_label = QLabel("25:00")
+        self.current_timer_label = QLabel("25:00")
 
-        row_1.addWidget(self.timer_label)
+        row_1.addWidget(self.current_timer_label)
         main_v_layout.addLayout(row_1)
 
 
@@ -84,48 +84,48 @@ class Pomodoro(QWidget):
         """Initialize the Pomodoro's timer."""
 
         self.run = False
-        self.time = 0
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.display_time)
-        self.timer.start(1000)
+        self.current_time = 0
+        self.current_timer = QTimer()
+        self.current_timer.timeout.connect(self.display_time)
+        self.current_timer.start(1000)
 
 
     def display_time(self):
         """Function to display the time."""
 
-        self.time_min = self.time // 60
-        self.time_sec = self.time % 60
+        self.current_time_min = self.current_time // 60
+        self.current_time_sec = self.current_time % 60
 
         # when minutes and seconds reaches single-digit values it will 
         # not display a zero before the current digit therefore this 
         # statement will add zero before the digit.
-        if self.time_sec < 10:
-            self.time_sec = f"0{self.time_sec}"
-        if self.time_min < 10:
-            self.time_min = f"0{self.time_min}"
+        if self.current_time_sec < 10:
+            self.current_time_sec = f"0{self.current_time_sec}"
+        if self.current_time_min < 10:
+            self.current_time_min = f"0{self.current_time_min}"
 
-        # variable to be used for displaying
-        self.time_str = f"{self.time_min}:{self.time_sec}"
+        # variable to be used to store time as a string
+        self.current_time_str = f"{self.current_time_min}:{self.current_time_sec}"
 
         # must display time when user clicked the start button
-        if self.run and self.time >= 0:
-            self.time -=1
-            self.timer_label.setText(self.time_str)
+        if self.run and self.current_time >= 0:
+            self.current_time -=1
+            self.current_timer_label.setText(self.current_time_str)
 
         # stop running upon displaying zero
-        elif self.time < 0:
+        elif self.current_time < 0:
             self.run = False
 
 
     def start(self):
         """Handles starting of the pomodoro timer"""
         
-        self.time = STUDY_TIME_SEC
+        self.current_time = STUDY_TIME_SEC
         self.run = True
 
 
     def stop(self):
         """Handles stopping of the pomodoro timer"""
 
-        self.time = STUDY_TIME_SEC
+        self.current_time = STUDY_TIME_SEC
         self.run = False
