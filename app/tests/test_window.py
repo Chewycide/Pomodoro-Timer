ba3c5d0e8244d9d7cb595b4ca2d843d1529f2912
Chewycide <gitchewycide@gmail.com>
2023-02-02 11:37:14 +0800

    Update And Refactor Test Case For Pomodoro Function
    Added pytest fixtures to prepare app for testing and added a few cases
    to test for the text to string function.

--- app/tests/test_window.py
+++ app/tests/test_window.py
@@ -1,11 +1,29 @@
+import pytest
 from app.build.window import Pomodoro
 
-def test_window(qtbot):
-    """TODO: group tests into class"""
+
+@pytest.fixture
+def app(qtbot):
+    """Arrange/Prepare Pomodoro app for testing"""
     pomodoro = Pomodoro()
     qtbot.addWidget(pomodoro)
+    return pomodoro
 
-    pomodoro.current_time = 1500
-    return_str = pomodoro.time_to_string()
-    print("test")
+
+def test_time_to_string(app):
+    
+    app.current_time = 1500
+    return_str = app.time_to_string()
     assert return_str == "25:00"
+
+    app.current_time = 1400
+    return_str = app.time_to_string()
+    assert return_str == "23:20"
+
+    app.current_time = 0
+    return_str = app.time_to_string()
+    assert return_str == "00:00"
+
+    app.current_time = 599
+    return_str = app.time_to_string()
+    assert return_str == "09:59"
