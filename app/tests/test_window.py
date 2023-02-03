4fc92acbe4129599b873e63dec612cccad7327fa
Chewycide <gitchewycide@gmail.com>
2023-02-03 17:55:40 +0800

    Add Various Test Functions
    - test start
    - test resume
    - test pause
    - test timer logic

--- app/tests/test_window.py
+++ app/tests/test_window.py
@@ -10,7 +10,7 @@ def app(qtbot):
     return pomodoro
 
 
-def test_time_to_string(app):
+def test_time_to_string(app: Pomodoro):
     
     app.current_time = 1500
     return_str = app.time_to_string()
@@ -27,3 +27,28 @@ def test_time_to_string(app):
     app.current_time = 599
     return_str = app.time_to_string()
     assert return_str == "09:59"
+
+
+def test_start_resume(app: Pomodoro):
+
+    app.isPaused = False
+    app.start()
+    assert app.stop_btn.isEnabled() == True
+    assert app.study_time_btn.isEnabled() == False
+    assert app.short_break_btn.isEnabled() == False
+    assert app.long_break_btn.isEnabled() == False
+    assert app.run == True
+
+
+def test_pause(app: Pomodoro):
+
+    app.isPaused = True
+    app.start()
+    assert app.run == False
+
+
+def test_timer_logic(app: Pomodoro):
+
+    app.current_time = -1
+    app.display_time()
+    assert app.run == False
